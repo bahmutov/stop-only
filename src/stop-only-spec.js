4afb71ccedc1cd60b2ed6ff96c18aea3573d08d8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-11 12:54:10 -0400

    initial

---
+++ src/stop-only-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const stopOnly = require('.')
+
+describe('stop-only', () => {
+  it('write this test', () => {
+    console.assert(stopOnly, 'should export something')
+  })
+})
